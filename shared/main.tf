f933fb75892944f7003cfff63ddaf1c7475b7b68
Julio Colon <julio.colon@microsoft.com>
2019-03-25 15:12:45 -0400

    added new naming conventions

--- shared/main.tf
+++ shared/main.tf
@@ -4,16 +4,39 @@ module "azure-provider" {
 
 locals {
   location_suffixes = {
-    centralus = "cus"
-    eastus = "eus"
-    eastus2 = "eus2"
-    westus = "wus"
-    northcentralus = "ncus"
-    southcentralus = "scus"
-    westcentralus = "wcus"
-    westus2 = "wus2"
+    eastasia = "asea",
+    southeastasia	= "assw",
+    centralus	= "usce",
+    eastus = "usea",
+    eastus2	= "use2",
+    westus = "uswe",
+    westus2	= "usw2",
+    northcentralus = "usnc",
+    southcentralus = "ussc",
+    westcentralus	= "uswc",
+    northeurope	= "euno",
+    westeurope	= "euwe",
+    japanwest	= "jawe",
+    japaneast	= "jaea",
+    brazilsouth	= "brso",
+    australiaeast	= "auea",
+    australiasoutheast = "ause",
+    southindia	= "inso",
+    centralindia	= "ince",
+    westindia	= "inwe",
+    canadacentral	= "cace",
+    canadaeast = "caea",
+    uksouth	= "ukso",
+    ukwest = "ukwe",
+    koreacentral = "koce",
+    koreasouth	= "koso",
+    francecentral	= "frce",
+    francesouth	= "frso",
+    australiacentral = "auce",
+    australiacentral2	= "auc2",
+    southafricanorth= "sano",
+    southafricawest	= "sawe",
   }
-
   location_suffix = "${local.location_suffixes[var.location]}"
   suffix = "${var.app_name}-${var.env}-${local.location_suffix}-${var.org}"
 }
