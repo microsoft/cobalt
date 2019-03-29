1cc4e0ccc52e13652e670eea7d93f58bbcbb3252
manojvazirani <mavazira@microsoft.com>
2019-03-29 11:25:02 -0400

    Integrate review comments for spacing

--- shared/main.tf
+++ shared/main.tf
@@ -8,45 +8,45 @@ terraform {
 
 locals {
   location_suffixes = {
-    eastasia = "asea",
-    southeastasia = "assw",
-    centralus = "usce",
-    eastus = "usea",
-    eastus2 = "use2",
-    westus = "uswe",
-    westus2 = "usw2",
-    northcentralus = "usnc",
-    southcentralus = "ussc",
-    westcentralus = "uswc",
-    northeurope = "euno",
-    westeurope = "euwe",
-    japanwest = "jawe",
-    japaneast = "jaea",
-    brazilsouth = "brso",
-    australiaeast = "auea",
+    eastasia           = "asea",
+    southeastasia      = "assw",
+    centralus          = "usce",
+    eastus             = "usea",
+    eastus2            = "use2",
+    westus             = "uswe",
+    westus2            = "usw2",
+    northcentralus     = "usnc",
+    southcentralus     = "ussc",
+    westcentralus      = "uswc",
+    northeurope        = "euno",
+    westeurope         = "euwe",
+    japanwest          = "jawe",
+    japaneast          = "jaea",
+    brazilsouth        = "brso",
+    australiaeast      = "auea",
     australiasoutheast = "ause",
-    southindia = "inso",
-    centralindia = "ince",
-    westindia = "inwe",
-    canadacentral = "cace",
-    canadaeast = "caea",
-    uksouth = "ukso",
-    ukwest = "ukwe",
-    koreacentral = "koce",
-    koreasouth = "koso",
-    francecentral = "frce",
-    francesouth = "frso",
-    australiacentral = "auce",
-    australiacentral2 = "auc2",
-    southafricanorth= "sano",
-    southafricawest = "sawe",
+    southindia         = "inso",
+    centralindia       = "ince",
+    westindia          = "inwe",
+    canadacentral      = "cace",
+    canadaeast         = "caea",
+    uksouth            = "ukso",
+    ukwest             = "ukwe",
+    koreacentral       = "koce",
+    koreasouth         = "koso",
+    francecentral      = "frce",
+    francesouth        = "frso",
+    australiacentral   = "auce",
+    australiacentral2  = "auc2",
+    southafricanorth   = "sano",
+    southafricawest    = "sawe",
   }
 
   location_suffix = "${local.location_suffixes[var.location]}"
 }
 
 resource "azurerm_resource_group" "rg_core" {
-  name = "core-${local.location_suffix}-rg-${var.company}"
+  name     = "core-${local.location_suffix}-rg-${var.company}"
   location = "${var.location}"
 }
 
