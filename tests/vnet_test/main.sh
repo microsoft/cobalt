05ed62efd4feed5eba485a0fa291064305207650
manojvazirani <mavazira@microsoft.com>
2019-04-01 16:35:02 -0400

    Add tests for virtual network and subnet

---
+++ tests/vnet_test/main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+. ../assert.sh continue_on_error #  load test functions with continue_on_error/stop_on_error
+
+export TF_VAR_location=eastus
+export TF_VAR_company=test
+
+#Check if resource group exists
+#Resource group does not exists at this point
+assertRG core-usea-rg-test not
+
+#Deploy all the test resources
+terraform init ../../shared/
+terraform apply -auto-approve ../../shared/
+
+#Check if resource group exists
+assertRG core-usea-rg-test
+
+#Check if virtual network exists
+assertResource core-usea-rg-test "Microsoft.Network/virtualNetworks" core-usea-vnet-test
+
+#Check if subnet exists inside virtual network
+assertSubnet core-usea-rg-test core-usea-vnet-test core-usea-subnet-test
+
+#Destroy the test resources
+terraform destroy -auto-approve ../../shared/
