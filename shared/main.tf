2db7a29c8df0b2bbfa15b3bf961803697a578884
manojvazirani <mavazira@microsoft.com>
2019-04-01 11:41:17 -0400

    Add virtual network & subnet to terraform template

--- shared/main.tf
+++ shared/main.tf
@@ -68,3 +68,17 @@ resource "azurerm_key_vault" "keyvault" {
     bypass         = "AzureServices"
   }
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                 = "core-${local.location_suffix}-vnet-${var.company}"
+  address_space        = ["10.0.0.0/16"]
+  location             = "${azurerm_resource_group.rg_core.location}"
+  resource_group_name  = "${azurerm_resource_group.rg_core.name}"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "core-${local.location_suffix}-subnet-${var.company}"
+  resource_group_name  = "${azurerm_resource_group.rg_core.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "10.0.1.0/24"
+}
