9173ec54181cb4d7b0b106e6909e935c7481f139
Rick Rainey <rickra@microsoft.com>
2019-04-02 11:14:29 -0500

    Added logic to clean up test resources.

--- tests/acr-sp-init-test.sh
+++ tests/acr-sp-init-test.sh
@@ -52,15 +52,16 @@ fi
 
 # Test service principals
 echo "Testing service principals..."
+
+# Get the ACR ID for the container registry the service principal role assignments should be scoped to.
+acrId=$(az acr show --name $acrName --query id)
+acrId="${acrId//\"}"
+
 for spName in ${!spAcrNameAndRole[@]}
 do
     # Get the appId of the expected service principal
     spAppId=$(az ad sp show --id ${spName} --query appId)
     spAppId="${spAppId//\"}"
-
-    # Get the ACR ID for the container registry the service principal role assignments should be scoped to.
-    acrId=$(az acr show --name $acrName --query id)
-    acrId="${acrId//\"}"
 
     # Get the role assignment scoped to the ACR for the service principal.
     roleAssignment=$(az role assignment list --assignee ${spName} --scope ${acrId} --role ${spAcrNameAndRole[$spName]} --query 'length(@)')
@@ -71,6 +72,27 @@ do
 done
 
 echo "Tests passed successfully"
+
+# Clean up tests results
+echo "Cleaning up test resources"
+
+echo "  Cleaning up service principals and role assignments..."
+for spName in ${!spAcrNameAndRole[@]}
+do
+    # Clean up service principals and role assignments
+    spAppId=$(az ad sp show --id ${spName} --query appId)
+    spAppId="${spAppId//\"}"
+    az ad sp delete --id ${spAppId}
+done
+
+# Clean up container registry
+echo "  Cleaning up container registry..."
+az acr delete --name ${acrName}
+
+# Clean up resource group
+echo "  Cleaning up resource group..."
+az group delete --name ${rgName} --yes
+
 
 
 
