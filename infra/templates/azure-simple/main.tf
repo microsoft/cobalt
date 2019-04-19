100ded196eea5fd6e3f5300173d52e5d58617358
Julio Colon <julio.colon@microsoft.com>
2019-04-19 11:51:14 -0400

    Scafolding

---
+++ infra/templates/azure-simple/main.tf
@@ -0,0 +1,66 @@
+module "provider" {
+  source = "../../modules/providers/azure/provider"
+}
+
+resource "azurerm_resource_group" "cluster_rg" {
+  name     = "${var.resource_group_name}"
+  location = "${var.resource_group_location}"
+
+  tags = {
+    environment = "${var.name}-single-region"
+  }
+}
+
+module "app_gateway" {
+  source                  = "../../modules/providers/azure/app-gateway"
+}
+
+module "service_plan" {
+  source              = "../../modules/providers/azure/service-plan"
+  resource_group_name = "${var.resource_group_name}"
+  resource_group_location = "${var.resource_group_location}"
+  service_plan_name = "${var.service_plan_name}"
+  app_service_name = "${var.app_service_name}"
+}
+
+module "apimanagement" {
+  source = "../../modules/providers/azure/api-mgmt"
+}
+
+module "backend_state" {
+  source   = "github.com/Microsoft/bedrock/cluster/azure/backend-state"
+  location = "${var.resource_group_location}"
+}
+
+module "vnet" {
+  source                  = "github.com/Microsoft/bedrock/cluster/azure/vnet"
+  vnet_name               = "${var.vnet_name}"
+  address_space           = "${var.address_space}"
+  resource_group_name     = "${var.resource_group_name}"
+  resource_group_location = "${var.resource_group_location}"
+  subnet_names            = ["${var.cluster_name}-subnet"]
+  subnet_prefixes         = ["${var.subnet_prefixes}"]
+
+  tags = {
+    environment = "${var.name}-single-region"
+  }
+}
+
+module "traffic_manager_profile" {
+  source                       = "github.com/Microsoft/bedrock/cluster/azure/tm-profile"
+  traffic_manager_profile_name = "${var.traffic_manager_profile_name}"
+  traffic_manager_dns_name     = "${var.traffic_manager_dns_name}"
+  resource_group_name          = "${var.resource_group_name}"
+  resource_group_location      = "${var.resource_group_location}"
+}
+
+module "traffic_manager_endpoint" {
+  source                              = "github.com/Microsoft/bedrock/cluster/azure/tm-endpoint-ip"
+  resource_group_name                 = "${var.resource_group_name}"
+  resource_location                   = "${var.resource_location}"
+  traffic_manager_profile_name        = "${var.traffic_manager_profile_name}"
+  traffic_manager_resource_group_name = "${var.traffic_manager_resource_group_name}"
+  public_ip_name                      = "${var.public_ip_name}"
+  endpoint_name                       = "${var.endpoint_name}"
+  ip_address_out_filename             = "${var.ip_address_out_filename}"
+}
---
+++ infra/templates/azure-simple/variables.tf
@@ -0,0 +1,109 @@
+# Azure Container Registry
+
+variable "acr_enabled" {
+  type    = "string"
+  default = "true"
+}
+
+# Resource Group
+
+variable "resource_group_name" {
+  type = "string"
+}
+
+variable "resource_group_location" {
+  type = "string"
+}
+
+variable "name" {
+  type = "string"
+}
+
+# Service Principal
+
+variable "service_principal_id" {
+  type = "string"
+}
+
+variable "service_principal_secret" {
+  type = "string"
+}
+
+variable "service_plan_name" {
+  type = "string"
+}
+
+variable "app_service_name" {
+  type = "string"
+}
+
+# vnet
+
+variable "vnet_name" {
+  type = "string"
+}
+
+variable "cluster_name" {
+  type = "string"
+}
+
+variable "service_cidr" {
+  default     = "10.0.0.0/16"
+  description = "Used to assign internal services in the AKS cluster an IP address. This IP address range should be an address space that isn't in use elsewhere in your network environment. This includes any on-premises network ranges if you connect, or plan to connect, your Azure virtual networks using Express Route or a Site-to-Site VPN connections."
+  type        = "string"
+}
+
+variable "dns_ip" {
+  default     = "10.0.0.10"
+  description = "should be the .10 address of your service IP address range"
+  type        = "string"
+}
+
+variable "docker_cidr" {
+  default     = "172.17.0.1/16"
+  description = "IP address (in CIDR notation) used as the Docker bridge IP address on nodes. Default of 172.17.0.1/16."
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network."
+  default     = "10.10.0.0/16"
+}
+
+variable "subnet_names" {
+  description = "A list of public subnets inside the vNet."
+  default     = ["cobalt-subnet1", "cobalt-subnet2"]
+}
+
+variable "subnet_prefixes" {
+  description = "The address prefix to use for the subnet."
+  default     = ["10.0.1.0/24"]
+}
+
+# Traffic Manager Variables
+variable "traffic_manager_profile_name" {
+  description = "Profile name for the trafic manager"
+}
+
+variable "traffic_manager_dns_name" {
+  description = "DNS name that will be given to the traffic manager"
+}
+
+variable "resource_location" {
+  description = "Traffic manager resource location"
+}
+
+variable "traffic_manager_resource_group_name" {
+  description = "Resource manager that will be used to group the traffic manager"
+}
+
+variable "public_ip_name" {
+  description = "IP address that will be given to the traffic manager"
+}
+
+variable "endpoint_name" {
+  description = "Name of the endpoint that will be associate to the traffic manager"
+}
+
+variable "ip_address_out_filename" {
+  description = "File that will be used to report the IP address of the traffic manager"
+}
