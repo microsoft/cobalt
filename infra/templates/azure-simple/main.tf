dc16646a62ce97f5a330e403774dbee523cde6ba
Julio Colon <julio.colon@microsoft.com>
2019-04-22 18:25:23 -0400

    variables and modules integration

--- infra/templates/azure-simple/main.tf
+++ infra/templates/azure-simple/main.tf
@@ -12,24 +12,24 @@ module "app_gateway" {
 }
 
 module "app_service" {
-  source = ""
+  source = "../../modules/providers/azure/service-plan"
 }
 
 module "service_plan" {
-  source = ""
+  source = "../../modules/providers/azure/service-plan"
 }
 
 module "apimanagement" {
-  source = ""
+  source = "../../modules/providers/azure/api-mgmt"
 }
 
 module "backend_state" {
-  source = "github.com/Microsoft/bedrock/cluster/azure/backend-state"
+  source   = "github.com/Microsoft/bedrock/cluster/azure/backend-state"
+  location = "${var.resource_group_location}"
 }
 
 module "vnet" {
-  source = "github.com/Microsoft/bedrock/cluster/azure/vnet"
-
+  source                  = "github.com/Microsoft/bedrock/cluster/azure/vnet"
   vnet_name               = "${var.vnet_name}"
   address_space           = "${var.address_space}"
   resource_group_name     = "${var.resource_group_name}"
@@ -41,10 +41,22 @@ module "vnet" {
     environment = "single-region"
   }
 }
+
 module "traffic_manager_profile" {
-  source = "github.com/Microsoft/bedrock/cluster/azure/tm-profile"
+  source                       = "github.com/Microsoft/bedrock/cluster/azure/tm-profile"
+  traffic_manager_profile_name = "${var.traffic_manager_profile_name}"
+  traffic_manager_dns_name     = "${var.traffic_manager_dns_name}"
+  resource_group_name          = "${var.resource_group_name}"
+  resource_group_location      = "${var.resource_group_location}"
 }
 
 module "traffic_manager_endpoint" {
-  source = "github.com/Microsoft/bedrock/cluster/azure/tm-endpoint-ip"
+  source                              = "github.com/Microsoft/bedrock/cluster/azure/tm-endpoint-ip"
+  resource_group_name                 = "${var.resource_group_name}"
+  resource_location                   = "${var.resource_location}"
+  traffic_manager_profile_name        = "${var.traffic_manager_profile_name}"
+  traffic_manager_resource_group_name = "${var.traffic_manager_resource_group_name}"
+  public_ip_name                      = "${var.public_ip_name}"
+  endpoint_name                       = "${var.endpoint_name}"
+  ip_address_out_filename             = "${var.ip_address_out_filename}"
 }
