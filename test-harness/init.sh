cadcfe6c26dab92f57ed2bfeead14ecba8a1373d
Erik Schlegel <erik.schlegel@gmail.com>
2019-05-01 15:25:37 -0400

    Integrated bashwizard to project

--- test-harness/init.sh
+++ test-harness/init.sh
@@ -14,10 +14,30 @@ declare BUILD_TEST_RUN_IMAGE="cobalt-test-harness"
 declare readonly TEMPLATE_DIR="infra/templates"
 declare readonly GIT_DIFF_EXTENSION_WHITE_LIST="*.go|*.tf|*.sh|Dockerfile*|*.tfvars"
 
+function echoError() {
+    RED=$(tput setaf 1)
+    NORMAL=$(tput sgr0)
+    echo "${RED}${1}${NORMAL}"
+}
+function echoWarning() {
+    YELLOW=$(tput setaf 3)
+    NORMAL=$(tput sgr0)
+    echo "${YELLOW}${1}${NORMAL}"
+}
+function echoInfo() {
+    GREEN=$(tput setaf 2)
+    NORMAL=$(tput sgr0)
+    echo "${GREEN}${1}${NORMAL}"
+}
+function echoIfVerbose() {
+    if [[ "$verbose" == true ]]; then
+        echo "${@}"
+    fi
+}
 
 function rebuild_test_image() {
     declare base_image=$1
-    echo "INFO: Using base image tag $base_image"
+    echoInfo "INFO: Using base image tag $base_image"
     docker build --rm -f "test-harness/Dockerfile" \
         -t $BUILD_TEST_RUN_IMAGE:$BUILD_BUILDID . \
         --build-arg build_directory="$BUILD_TEMPLATE_DIRS" \
@@ -25,18 +45,18 @@ function rebuild_test_image() {
 }
 
 function check_required_env_variables() {
-    echo "INFO: Checking required environment variables"
+    echoInfo "INFO: Checking required environment variables"
     for var in BUILD_BUILDID ARM_SUBSCRIPTION_ID ARM_CLIENT_ID ARM_CLIENT_SECRET ARM_TENANT_ID ARM_ACCESS_KEY ; do
         if [[ ! -v ${var} ]] ; then
-            echo "ERROR: $var is not set in the environment"
+            echoError "ERROR: $var is not set in the environment"
             return 0
         fi
     done
-    echo "INFO: passed environment variable check"
+    echoInfo "INFO: passed environment variable check"
 }
 
 function default_to_all_template_paths() {
-    echo "INFO: Terraform module file(s) changed. Running all tests"
+    echoInfo "INFO: Terraform module file(s) changed. Running all tests"
 	declare -a ALL_TEMPLATE_DIRS=(`find $TEMPLATE_DIR/* -maxdepth 0 -type d`)
     shopt -s nullglob
     for folder in "${ALL_TEMPLATE_DIRS[@]}"
@@ -55,8 +75,8 @@ function add_template_if_not_exists() {
 }
 
 function load_build_directory() {
-    template_dirs=$( IFS=$' '; echo "${TEST_RUN_MAP[*]}" )
-    echo "INFO: Running local build for templates: $template_dirs"
+    template_dirs=$( IFS=$' '; echoInfo "${TEST_RUN_MAP[*]}" )
+    echoInfo "INFO: Running local build for templates: $template_dirs"
     mkdir $BUILD_TEMPLATE_DIRS && cp -r $template_dirs *.go $BUILD_TEMPLATE_DIRS
 }
 
@@ -64,9 +84,9 @@ function build_test_harness() {
     GIT_DIFF_UPSTREAMBRANCH=$1
     GIT_DIFF_SOURCEBRANCH=$2
     BASE_IMAGE=$3
-    echo "INFO: verified that environment is fully defined"
+    echoInfo "INFO: verified that environment is fully defined"
     template_build_targets $GIT_DIFF_UPSTREAMBRANCH $GIT_DIFF_SOURCEBRANCH
-    echo "INFO: Building test harness image"
+    echoInfo "INFO: Building test harness image"
     rebuild_test_image $BASE_IMAGE
     if [ -d "$BUILD_TEMPLATE_DIRS" ]; then rm -Rf $BUILD_TEMPLATE_DIRS; fi
 }
@@ -74,11 +94,11 @@ function build_test_harness() {
 function template_build_targets() {
     GIT_DIFF_UPSTREAMBRANCH=$1
     GIT_DIFF_SOURCEBRANCH=$2
-    [[ -z $GIT_DIFF_UPSTREAMBRANCH ]] && echo "ERROR: GIT_DIFF_UPSTREAMBRANCH wasn't provided" && return 1
+    [[ -z $GIT_DIFF_UPSTREAMBRANCH ]] && echoError "ERROR: GIT_DIFF_UPSTREAMBRANCH wasn't provided" && return 1
 
-    [[ -z $GIT_DIFF_SOURCEBRANCH ]] && echo "ERROR: GIT_DIFF_SOURCEBRANCH wasn't provided" && return 1
+    [[ -z $GIT_DIFF_SOURCEBRANCH ]] && echoError "ERROR: GIT_DIFF_SOURCEBRANCH wasn't provided" && return 1
 
-    echo "INFO: Running git diff from branch ${GIT_DIFF_SOURCEBRANCH}"
+    echoInfo "INFO: Running git diff from branch ${GIT_DIFF_SOURCEBRANCH}"
     files=(`git diff ${GIT_DIFF_UPSTREAMBRANCH} ${GIT_DIFF_SOURCEBRANCH} --name-only|grep -E ${GIT_DIFF_EXTENSION_WHITE_LIST}||true`)
     for file in "${files[@]}"
     do
@@ -104,7 +124,7 @@ function template_build_targets() {
     done
 
     if [ ${#TEST_RUN_MAP[@]} -eq 0 ]; then
-        echo "INFO: No templates to process. Exiting build step"
+        echoWarning "INFO: No templates to process. Exiting build step"
         exit 0
     fi
 
--- test-harness/local-run.sh
+++ test-harness/local-run.sh
@@ -1,19 +1,48 @@
-# NAME: local-run.sh
-# DESCRIPTION: 
-# Builds and runs the test harness container. This container runs all build target tasks on the 
-# host machine. These targets include mage clean, format, unit and integration tests.
-# This base image also pre-installs the golang vendor
-# USAGE: ./test-harness/local-run.sh
+#!/bin/bash
+#---------- see https://github.com/joelong01/BashWizard ----------------
+# bashWizard version 1.0.0
+# this will make the error text stand out in red - if you are looking at these errors/warnings in the log file
+# you can use cat <logFile> to see the text in color.
 
-#!/usr/bin/env bash
-set -euo pipefail
 . ./test-harness/init.sh --source-only
 
-readonly BUILD_SOURCEBRANCHNAME=`git branch | sed -n '/\* /s///p'`
-readonly BUILD_UPSTREAMBRANCH="master"
-readonly TF_STATE_STORAGE_ACCT="cobaltfstate"
-readonly TF_STATE_CONTAINER="cobaltfstate-remote-state-container"
-readonly docker_base_image_name="msftcse/cobalt-test-base"
+# make sure this version of *nix supports the right getopt
+! getopt --test 2>/dev/null
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echoError "'getopt --test' failed in this environment. please install getopt."
+    read -r -p "install getopt using brew? [y,n]" response
+    if [[ $response == 'y' ]] || [[ $response == 'Y' ]]; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+        brew install gnu-getopt
+        #shellcheck disable=SC2016
+        echo 'export PATH="/usr/local/opt/gnu-getopt/bin:$PATH"' >> ~/.bash_profile
+        echoWarning "you'll need to restart the shell instance to load the new path"
+    fi
+   exit 1
+fi
+
+function usage() {
+    
+    echo "Builds and runs the test harness container. This container runs all build target tasks on the host machine. These targets include mage clean, format, unit and integration tests. This base image also pre-installs the golang vendor. "
+    echo ""
+    echo "Usage: $0  -b|--Docker-Base-Image-Name -a|--Terraform-Remote-State-Storage-Account -c|--Terraform-Remote-State-Storage-Container " 1>&2
+    echo ""
+    echo " -b | --Docker-Base-Image-Name                       Optional     "
+    echo " -a | --Terraform-Remote-State-Storage-Account       Optional     "
+    echo " -c | --Terraform-Remote-State-Storage-Container     Optional     "
+    echo ""
+    exit 1
+}
+function echoInput() {
+    echo "local-run.sh:"
+    echo -n "    Docker-Base-Image-Name...................... "
+    echoInfo "$docker_base_image_name"
+    echo -n "    Terraform-Remote-State-Storage-Account...... "
+    echoInfo "$tf_state_storage_acct"
+    echo -n "    Terraform-Remote-State-Storage-Container.... "
+    echoInfo "$tf_state_container"
+
+}
 
 function dotenv() {
     set -a
@@ -21,31 +50,88 @@ function dotenv() {
     set +a
 }
 
+function parseInput() {
+
+    local OPTIONS=b:ac
+    local LONGOPTS=Docker-Base-Image-Name:,Terraform-Remote-State-Storage-Account,Terraform-Remote-State-Storage-Container
+
+    # -use ! and PIPESTATUS to get exit code with errexit set
+    # -temporarily store output to be able to check for errors
+    # -activate quoting/enhanced mode (e.g. by writing out "--options")
+    # -pass arguments only via -- "$@" to separate them correctly
+    ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        # e.g. return value is 1
+        # then getopt has complained about wrong arguments to stdout
+        usage
+        exit 2
+    fi
+    # read getopt's output this way to handle the quoting right:
+    eval set -- "$PARSED"
+    while true; do
+        case "$1" in
+        -b | --Docker-Base-Image-Name)
+            docker_base_image_name=$2
+            shift 2
+            ;;
+        -a | --Terraform-Remote-State-Storage-Account)
+            tf_state_storage_acct=$2
+            shift 2
+            ;;
+        -c | --Terraform-Remote-State-Storage-Container)
+            tf_state_container=$2
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echoError "Invalid option $1 $2"
+            exit 3
+            ;;
+        esac
+    done
+}
+
+# Bind environment from .env
+dotenv
+
+# input variables
+declare docker_base_image_tag="g${GO_VERSION}t${TF_VERSION}"
+declare docker_base_image_name="msftcse/cobalt-test-base:$docker_base_image_tag"
+declare tf_state_storage_acct="cobaltfstate"
+declare tf_state_container="cobaltfstate-remote-state-container"
+
+# Parse user input arguments
+parseInput "$@"
+
+readonly BUILD_SOURCEBRANCHNAME=`git branch | sed -n '/\* /s///p'`
+readonly BUILD_UPSTREAMBRANCH="master"
+
 function run_test_harness() {
-    echo "INFO: loading environment"
-    dotenv
+    echoInfo "INFO: loading environment"
     check_required_env_variables
-    docker_base_image_tag="g${GO_VERSION}t${TF_VERSION}"
-    echo "INFO: verified that environment is fully defined"
+    echoInfo "INFO: verified that environment is fully defined"
     build_test_harness $BUILD_UPSTREAMBRANCH \
                        $BUILD_SOURCEBRANCHNAME \
-                       "$docker_base_image_name:$docker_base_image_tag"
+                       $docker_base_image_name
     run_test_image
 }
 
 function run_test_image() {
-    echo "INFO: Running test harness container"
+    echoInfo "INFO: Running test harness container"
     docker run -e ARM_SUBSCRIPTION_ID=$ARM_SUBSCRIPTION_ID \
             -e ARM_CLIENT_ID=$ARM_CLIENT_ID \
             -e ARM_CLIENT_SECRET=$ARM_CLIENT_SECRET \
             -e ARM_TENANT_ID=$ARM_TENANT_ID \
             -e DATACENTER_LOCATION=$DATACENTER_LOCATION \
-            -e TF_STATE_STORAGE_ACCT=$TF_STATE_STORAGE_ACCT \
-            -e TF_STATE_CONTAINER=$TF_STATE_CONTAINER \
+            -e TF_STATE_STORAGE_ACCT=$tf_state_storage_acct \
+            -e TF_STATE_CONTAINER=$tf_state_container \
             -e ARM_ACCESS_KEY=$ARM_ACCESS_KEY \
             --rm $BUILD_TEST_RUN_IMAGE:$BUILD_BUILDID
 
-    echo "INFO: Completed test run"
+    echoInfo "INFO: Completed test run"
 }
 
 run_test_harness
