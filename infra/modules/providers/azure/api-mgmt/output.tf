958702ed056252891f247488abf90ab2fcf7d707
Erik Schlegel <erik.schlegel@gmail.com>
2019-05-14 17:22:28 -0500

    removing APIM terraform templates from an earlier commit

--- infra/modules/providers/azure/api-mgmt/output.tf
+++
@@ -1,9 +0,0 @@
-output "gatewayurl" {
-    description = "The url for managing the API manager"
-    value = "${lookup(azurerm_template_deployment.apimgmt.outputs, "gatewayurl")}"
-}
-
-output "api_url" {
-  description = "The ID of the API Management API."
-  value       = "${azurerm_api_management_api.apimgmt.*.id}"
-}
