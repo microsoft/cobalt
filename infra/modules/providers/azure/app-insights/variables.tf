8e17e5cf09ba843ec463e429318da1741ba95817
Amanda Kaufman <aka@microsoft.com>
2019-05-22 11:53:29 -0600

    Added more detailed descriptions for variables.  Removed default values to prompt for necessary information.

--- infra/modules/providers/azure/app-insights/variables.tf
+++ infra/modules/providers/azure/app-insights/variables.tf
@@ -4,9 +4,8 @@ variable "service_plan_resource_group_name" {
 }
 
 variable "resource_tags" {
-  description = "Map of tags to apply to taggable resources in this module.  By default the taggable resources are tagged with the name defined above and this map is merged in"
+  description = "Map of tags to apply to taggable resources in this module (enter as a set of curly braces containing key-value pairs, as in: {\"tag1\" = \"value1\", \"tag2\" = \"value2\"}).  By default the taggable resources are tagged with the name defined above and this map is merged in"
   type        = "map"
-  default     = {}
 }
 
 variable "appinsights_name" {
@@ -15,7 +14,6 @@ variable "appinsights_name" {
 }
 
 variable "appinsights_application_type" {
-  description = "Type of the App Insights Application"
+  description = "Type of the App Insights Application.  Valid values are ios for iOS, java for Java web, MobileCenter for App Center, Node.JS for Node.js, other for General, phone for Windows Phone, store for Windows Store and web for ASP.NET."
   type        = "string"
-  default     = "Web"
 }
