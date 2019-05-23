be18093760755d6a213d9f0ffc06f7b78762c5a4
Eric Peralli <eric.peralli@microsoft.com>
2019-05-23 10:30:26 -0600

    Marking primary access key output as sensitive

--- infra/modules/providers/azure/cosmosdb/output.tf
+++ infra/modules/providers/azure/cosmosdb/output.tf
@@ -4,6 +4,7 @@ output "cosmosdb_endpoint" {
 }
 
 output "cosmosdb_primary_master_key" {
-    description = "The Primary master key for the CosmosDB Account."
-    value = "${azurerm_cosmosdb_account.cosmosdb.primary_master_key}"
+  description = "The Primary master key for the CosmosDB Account."
+  value       = "${azurerm_cosmosdb_account.cosmosdb.primary_master_key}"
+  sensitive   = true
 }
