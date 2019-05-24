7222541284138bb21cede1520d93657bce2ee7bd
Erik Schlegel <erik.schlegel@gmail.com>
2019-05-24 14:45:57 -0500

    Updating readme

--- infra/templates/azure-simple/admin.tf
+++ infra/templates/azure-simple/admin.tf
@@ -1,11 +1,11 @@
 locals {
-  tm_profile_name     = "${var.name}-tf"
-  vnet_name           = "${var.name}-vnet"
+  tm_profile_name     = "${var.name}-${terraform.workspace}-tf"
+  vnet_name           = "${var.name}-${terraform.workspace}-vnet"
   tm_endpoint_name    = "${var.resource_group_location}_${var.name}"
   tm_dns_name         = "${var.name}-${terraform.workspace}-dns"
-  appgateway_name     = "${var.name}-gateway"
+  appgateway_name     = "${var.name}-${terraform.workspace}-gateway"
   public_pip_name     = "${var.name}-${terraform.workspace}-ip"
-  kv_name             = "${var.name}-kv"
+  kv_name             = "${var.name}-${terraform.workspace}-kv"
   resource_group_name = "${var.name}-${terraform.workspace}"
 }
 
