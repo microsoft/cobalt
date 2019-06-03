da8eec1e40e88e8a0cefb45140f0a3d906791b37
Eric Peralli <eric.peralli@microsoft.com>
2019-06-03 10:21:14 -0600

    Formatted terraform files

--- infra/modules/providers/azure/container-registry/main.tf
+++ infra/modules/providers/azure/container-registry/main.tf
@@ -3,10 +3,10 @@ data "azurerm_resource_group" "container_registry" {
 }
 
 resource "azurerm_container_registry" "container_registry" {
-  name                     = "${var.container_registry_name}"
-  resource_group_name      = "${data.azurerm_resource_group.container_registry.name}"
-  location                 = "${data.azurerm_resource_group.container_registry.location}"
-  sku                      = "${var.container_registry_sku}"
-  admin_enabled            = "${var.container_registry_admin_enabled}"
-  tags                     = "${var.container_registry_tags}"
+  name                = "${var.container_registry_name}"
+  resource_group_name = "${data.azurerm_resource_group.container_registry.name}"
+  location            = "${data.azurerm_resource_group.container_registry.location}"
+  sku                 = "${var.container_registry_sku}"
+  admin_enabled       = "${var.container_registry_admin_enabled}"
+  tags                = "${var.container_registry_tags}"
 }
--- infra/modules/providers/azure/container-registry/output.tf
+++ infra/modules/providers/azure/container-registry/output.tf
@@ -4,7 +4,7 @@ output "container_registry_id" {
 }
 
 output "container_registry_login_server" {
-  description = "The URL that can be used to log into the container registry."  
+  description = "The URL that can be used to log into the container registry."
   value       = "${azurerm_container_registry.container_registry.login_server}"
 }
 
