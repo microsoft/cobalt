e0b3acc391bf3430fb34690214d579dddcbd0abe
ericpera-msft <49953535+ericpera-msft@users.noreply.github.com>
2019-06-03 14:10:57 -0600

    Erperall/features/acr terraform module (#150)
    * Altered scripts to work on OSX
    
    * Moved YAML template file to root
    
    * Test
    
    * Revert
    
    * Removed YAML file from root
    
    * Removed caching from docker build command
    
    * Created ACR terraform module
    
    * Added a readme for ACR and changed the service plan reference to a data connection.
    
    * Added tags to module and changed naming scheme
    
    * Fixed some issues pointed out by Erik
    
    * Updated container registry documentation
    
    * Formatted terraform files

---
+++ infra/modules/providers/azure/container-registry/output.tf
@@ -0,0 +1,15 @@
+output "container_registry_id" {
+  description = "The Container Registry ID."
+  value       = "${azurerm_container_registry.container_registry.id}"
+}
+
+output "container_registry_login_server" {
+  description = "The URL that can be used to log into the container registry."
+  value       = "${azurerm_container_registry.container_registry.login_server}"
+}
+
+output "container_registry_admin_username" {
+  description = "The Username associated with the Container Registry Admin account - if the admin account is enabled."
+  value       = "${azurerm_container_registry.container_registry.admin_username}"
+  sensitive   = true
+}
