f9c5eeb87a0f8dd449b2833ab88f317b4b71e7a3
technicallywilliams <dexterwilliams04@gmail.com>
2019-06-20 07:54:25 -0500

    add service principal default display name

--- infra/modules/providers/azure/service-principal/variables.tf
+++ infra/modules/providers/azure/service-principal/variables.tf
@@ -1,20 +1,3 @@
-variable "create_for_rbac" {
-  description = "Create a new Service Principle"
-  type        = bool
-  default     = false
-}
-
-variable "display_name" {
-  description = "Display name of the AD application"
-  type        = string
-}
-
-variable "object_id" {
-  description = "Object Id of the service principle to assign a role"
-  type        = string
-  default     = ""
-}
-
 variable "role_name" {
   description = "The name of the role definition to assign a service principle too."
   type        = string
@@ -25,3 +8,20 @@ variable "role_scope" {
   type        = string
 }
 
+variable "create_for_rbac" {
+  description = "Create a new Service Principle"
+  type        = bool
+  default     = false
+}
+
+variable "display_name" {
+  description = "Display name of the AD application"
+  type        = string
+  default     = "AD Client"
+}
+
+variable "object_id" {
+  description = "Object Id of the service principle to assign a role"
+  type        = string
+  default     = ""
+}
