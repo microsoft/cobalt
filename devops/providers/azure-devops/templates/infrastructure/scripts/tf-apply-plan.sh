bcc411c4422bc823d7acdd27fb5f0f822233c729
erikschlegel <erik.schlegel@gmail.com>
2019-07-29 20:55:39 -0500

    fixing issues with build template

--- devops/providers/azure-devops/templates/infrastructure/scripts/tf-apply-plan.sh
+++ devops/providers/azure-devops/templates/infrastructure/scripts/tf-apply-plan.sh
@@ -13,7 +13,7 @@ chmod -R 752 .terraform
 export ARM_ACCESS_KEY=$(storageAccountPrimaryKey)
 export ARM_CLIENT_SECRET=$servicePrincipalKey
 export ARM_CLIENT_ID=$servicePrincipalId
-export ARM_TENANT_ID=$azureTenantId
+# export ARM_TENANT_ID=$azureTenantId
 
 TF_PLAN_FILE="${TF_WORKSPACE_NAME}_plan.out"
 
