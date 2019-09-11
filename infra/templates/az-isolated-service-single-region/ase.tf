65940bacd750a11b7326e189a61c270da7357cf7
Dexter Williams <dexterwilliams04@gmail.com>
2019-09-11 00:23:22 -0500

    Removed extra character from unauth of ase.tf

--- infra/templates/az-isolated-service-single-region/ase.tf
+++ infra/templates/az-isolated-service-single-region/ase.tf
@@ -63,7 +63,7 @@ module "app_service" {
   app_service_config = {
     for target in var.unauthn_deployment_targets :
     target.app_name => {
-      image = "${target.image_name}:${target.image_release_tag_prefix}}"
+      image = "${target.image_name}:${target.image_release_tag_prefix}"
     }
   }
   providers = {
