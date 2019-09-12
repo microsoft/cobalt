e6868360c8eb3d82983e6f9bb95978785d389af1
Stephen Henderson <stephen@bluegood.com>
2019-09-12 10:44:07 -0500

    Fixes secrets handling in app settings for ACR access (#278)
    * isolate the test env from others
    
    * no dbl-quotes when expansion needed? also more settings to populate
    
    * keep the Cobalt defaults for the example vars
    
    * implement workaround for keyvault versioning in app settings
    
    * Update unit tests with expected count
    
    * Removed extra character from unauth of ase.tf

--- infra/modules/providers/azure/keyvault-secret/main.tf
+++ infra/modules/providers/azure/keyvault-secret/main.tf
@@ -8,3 +8,10 @@ resource "azurerm_key_vault_secret" "secret" {
   value        = var.secrets[local.secret_names[count.index]]
   key_vault_id = var.keyvault_id
 }
+
+data "azurerm_key_vault_secret" "secrets" {
+  count        = length(var.secrets)
+  depends_on   = [azurerm_key_vault_secret.secret]
+  name         = local.secret_names[count.index]
+  key_vault_id = var.keyvault_id
+}
--- infra/templates/az-isolated-service-single-region/ase.tf
+++ infra/templates/az-isolated-service-single-region/ase.tf
@@ -55,14 +55,15 @@ module "app_service" {
   service_plan_name                = module.service_plan.service_plan_name
   service_plan_resource_group_name = azurerm_resource_group.admin_rg.name
   app_insights_instrumentation_key = module.app_insights.app_insights_instrumentation_key
+  vault_uri                        = module.keyvault.keyvault_uri
   azure_container_registry_name    = module.container_registry.container_registry_name
   docker_registry_server_url       = module.container_registry.container_registry_login_server
   docker_registry_server_username  = module.acr_service_principal_acrpull.service_principal_application_id
-  docker_registry_server_password  = format("@Microsoft.KeyVault(SecretUri=%s)", "module.acr_service_principal_password.keyvault_secret_ids[0]") #data.azurerm_key_vault_secret.acr_password.id)
+  docker_registry_server_password  = format("@Microsoft.KeyVault(SecretUri=%s)", module.acr_service_principal_password.keyvault_secret_attributes[0].id)
   app_service_config = {
     for target in var.unauthn_deployment_targets :
     target.app_name => {
-      image = "${target.image_name}:${target.image_release_tag_prefix}}"
+      image = "${target.image_name}:${target.image_release_tag_prefix}"
     }
   }
   providers = {
@@ -85,6 +86,7 @@ module "authn_app_service" {
   source                           = "../../modules/providers/azure/app-service"
   service_plan_name                = module.service_plan.service_plan_name
   service_plan_resource_group_name = azurerm_resource_group.admin_rg.name
+  vault_uri                        = module.keyvault.keyvault_uri
   app_insights_instrumentation_key = module.app_insights.app_insights_instrumentation_key
   azure_container_registry_name    = module.container_registry.container_registry_name
   docker_registry_server_url       = module.container_registry.container_registry_login_server
