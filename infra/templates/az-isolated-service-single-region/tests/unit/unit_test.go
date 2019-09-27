bb688c8cf364f2211b7a1b8ab51ab9fce4d35807
Stephen Henderson <stephen.henderson@microsoft.com>
2019-09-27 10:47:47 -0500

    Updating unit test counts

--- infra/templates/az-isolated-service-single-region/tests/unit/unit_test.go
+++ infra/templates/az-isolated-service-single-region/tests/unit/unit_test.go
@@ -157,7 +157,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:             tfOptions,
 		Workspace:             workspace,
 		PlanAssertions:        nil,
-		ExpectedResourceCount: 62,
+		ExpectedResourceCount: 63,
 		ExpectedResourceAttributeValues: infratests.ResourceDescription{
 			"azurerm_resource_group.app_rg":                                                                                            expectedAppDevResourceGroup,
 			"azurerm_resource_group.admin_rg":                                                                                          expectedAdminResourceGroup,
