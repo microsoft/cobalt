7cae0ba3c319dd938dac9a2b02a032500e787fa3
Stephen Henderson <stephen@bluegood.com>
2019-10-03 08:43:11 -0500

    lint fix

--- infra/templates/az-isolated-service-single-region/helper-ips/variables.tf
+++ infra/templates/az-isolated-service-single-region/helper-ips/variables.tf
@@ -1,6 +1,6 @@
 variable "comma_sep_ip_list" {
   description = "A comma-separated list of IPs and/or CIDR/IP ranges that will be converted to a TF list/array, e.g. \"1.1.1.1/32, 8.8.8.8/24\"."
-  type = string
+  type        = string
 }
 
 variable "tail_list" {
@@ -8,6 +8,6 @@ variable "tail_list" {
     A TF list that will be combined with the `comma_sep_ip_list`, if provided. Items
     from the `comma_sep_ip_list` will appear to the left of items from this `tail_list` value."
 HERE
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
