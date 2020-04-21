8a120c2f52dc364bbd3b47ba32b9621f4328b6c0
Stephen Henderson <stephen@bluegood.com>
2020-04-21 12:06:46 -0500

    document outputs

--- infra/templates/ado-bootstrap-iac-pipeline/outputs.tf
+++ infra/templates/ado-bootstrap-iac-pipeline/outputs.tf
@@ -1,9 +1,4 @@
-output "project_id" {
-  value = local.project_id
-}
-output "project_name" {
-  value = local.project_name
-}
 output "repo_clone_url" {
+  description="The Git repository where IaC templates are stored and watched for changes"
   value = azuredevops_git_repository.repo.web_url
 }
