c42e9f82daecce898afa36578235cb612b5e1113
helayoty <hebaelayoty@gmail.com>
2020-07-30 09:45:25 -0700

    Add variables description

--- infra/modules/providers/devops/variable-group/variables.tf
+++ infra/modules/providers/devops/variable-group/variables.tf
@@ -1,23 +1,23 @@
 variable "project_name" {
   type        = string
-  description = ""
+  description = "Azure DevOps project name that will be used to add the new variable group"
 }
 
 variable "variable_group_name" {
   type        = string
-  description = ""
+  description = "Azure DevOps Variable Group Name to be created"
 }
 
 
 variable "variable_group_description" {
   type        = string
   description = ""
-  default     = " "
+  default     = "An optional field to add description about the variable group"
 }
 
 variable "allow_access" {
   type        = bool
-  description = ""
+  description = "A flag to detirmine if the variable group will allow to access all pipelines or not"
   default     = true
 }
 
