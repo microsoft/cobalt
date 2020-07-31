0e52cb6a965c6f0ecfccea8d9e3f874126d12579
Mahsa Hanifi <mahanifi@microsoft.com>
2020-07-30 17:12:34 -0700

    deleted data factory

--- infra/modules/providers/azure/data-factory/datasets.tf
+++
@@ -1,8 +0,0 @@
-
-resource "azurerm_data_factory_dataset_sql_server_table" "main" {
-  name                = var.data_factory_dataset_sql_name
-  resource_group_name = data.azurerm_resource_group.main.name
-  data_factory_name   = azurerm_data_factory.main.name
-  linked_service_name = azurerm_data_factory_linked_service_sql_server.main.name
-  table_name          = var.data_factory_dataset_sql_table_name
-}
--- infra/modules/providers/azure/data-factory/versions.tf
+++
@@ -1,9 +0,0 @@
-
-terraform {
-  required_version = ">= 0.12"
-}
-
-provider "azurerm" {
-  version = "~>2.6.0"
-  features {}
-}
